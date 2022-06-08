51e148a4c21d5b2411852331cdcd197f42774e09
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-06-07 23:48:27 -0700

    added delete and new post js

---
+++ public/js/newpost.js
@@ -0,0 +1,33 @@
+// New Post Form Handler
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  // Get the post title and post text from the form
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_text = document.querySelector('textarea[name="post-text"]').value.trim();
+
+
+
+  if (title && post_text){
+    const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      post_text
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  // if the response is okay, reload the page, showing the newest post now in the user's post list
+  if (response.ok) {
+    document.location.replace('/dashboard');
+    // otherwise, display the error
+  } else {
+    alert(response.statusText);
+  }
+}
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
